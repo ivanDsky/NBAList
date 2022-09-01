5f3467d3e645cecf87b493c8ba2a39e2e85361cd
zloydi <dobrovolskyivan2002@gmail.com>
2022-09-01 21:22:10 +0200

    Added view model factory for dagger

---
+++ di/src/main/java/ua/zloydi/di/ViewModelFactory.kt
@@ -0,0 +1,20 @@
+package ua.zloydi.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.MapKey
+import javax.inject.Provider
+import kotlin.reflect.KClass
+
+typealias ViewModelMap = @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+
+@MapKey
+annotation class ViewModelKey(val key: KClass<out ViewModel>)
+
+class ViewModelFactory(
+	private val viewModelMap: ViewModelMap
+) : ViewModelProvider.Factory {
+	override fun <T : ViewModel> create(modelClass: Class<T>): T {
+		return (viewModelMap[modelClass]?.get() as? T) ?: error("No viewModel found")
+	}
+}
